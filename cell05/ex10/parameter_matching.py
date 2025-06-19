b868d0e070c5bc2e00d17c0a028abc4c3b21071f
panyaphon06 <68030171@kmitl.ac.th>
2025-06-19 06:56:53 +0000

    Do it a again 09

--- cell05/ex10/parameter_matching.py
+++ cell05/ex10/parameter_matching.py
@@ -1,1 +1,13 @@
-print("RTFM (Read the F-ing manual)".lower())
+#!/usr/bin/env python3
+import sys
+
+if len(sys.argv) != 2:
+    print("none")
+else:
+    expected_word = sys.argv[1]
+    user_input = input("What was the parameter? ")
+    if user_input == expected_word:
+        print("Good job!")
+    else:
+        print("Nope, sorry...")
+
