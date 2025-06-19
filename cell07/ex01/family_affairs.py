e59731d8961de5738040b522a52c73cb52d441b9
panyaphon06 <68030171@kmitl.ac.th>
2025-06-19 03:50:00 +0000

    Do it a again 06

--- cell07/ex01/family_affairs.py
+++ cell07/ex01/family_affairs.py
@@ -1,12 +1,12 @@
 def genopum (family):
-    return list(filter(lambda name: family[name] == "pum", family.keys()))
+    return list(filter (lambda name: family[name] == "pum", family.keys()))
 
 dupont_family = {
-"pum": "pum",
-"geno": "geno",
-"mairu": "tem",
-"chutinun": "pum",
-"yungmak": "pum"
+    "pum": "pum",
+    "geno": "geno",
+    "mairu": "tem",
+    "chutinun": "pum",
+    "yungmak":"pum"
 }
 
 print(genopum (dupont_family))
